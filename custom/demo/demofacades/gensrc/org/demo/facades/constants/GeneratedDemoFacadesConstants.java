c553772de0c6a38591480557e9ec02635d47a375
resacahya <resa.cahyasaputra@aai.astra.co.id>
2020-06-22 22:26:56 +0700

    first-init

---
+++ custom/demo/demofacades/gensrc/org/demo/facades/constants/GeneratedDemoFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jun 22, 2020, 7:51:50 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.demo.facades.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedDemoFacadesConstants
+{
+	public static final String EXTENSIONNAME = "demofacades";
+	
+	protected GeneratedDemoFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
